0350904d3f4f50d1730b60df674cf4df46f14464
l0mb4x <marios.manis@hotmail.com>
2023-12-29 15:30:31 -0500

    Picture services

--- bin/setup.sh
+++ bin/setup.sh
@@ -8,7 +8,7 @@ sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y python3.8-venv
 
 echo "Making Python 3.8 the default..."
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8
 
 echo "Checking the Python version..."
 python3 --version
